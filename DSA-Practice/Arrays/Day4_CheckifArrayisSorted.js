2c23f26a958e942a570cf35864980c8add3f5a16
MaheshMedia <maheshnaik758@gmail.com>
2026-03-01 22:05:53 +0530

    Day4

--- DSA-Practice/Arrays/Day4_CheckifArrayisSorted.js
+++ DSA-Practice/Arrays/Day4_CheckifArrayisSorted.js
@@ -1,1 +1,13 @@
 //Problem 4: Check if Array is Sorted: Given an array, check whether it is sorted in ascending order
+function isSorted(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < arr[i - 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Example
+console.log(isSorted([1, 2, 3, 4, 5]));   // true
+console.log(isSorted([1, 3, 2, 4, 5]));   // false
